81d1d4a44992171c511601c4cb1a20d700b02a54
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-04-07 19:25:06 +0700

    chore: add tsdown.config.ts + README symlink

--- packages/lintmax/tsdown.config.ts
+++ packages/lintmax/tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ['src/index.ts', 'src/cli.ts', 'src/eslint.ts'],
+  entry: ['src/index.ts', 'src/eslint.ts', 'src/cli.ts'],
   format: 'esm',
   outDir: 'dist'
 })
