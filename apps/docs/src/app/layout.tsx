0ec68b2421d075a158abb7aaa579e622c5f372cb
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-04-08 23:56:09 +0700

    fix: use array join for font className to avoid cn/no-template-literal

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,9 +1,10 @@
 import { mono, sans } from './fonts'
 import './global.css'
 import { Providers } from './providers'
+const fonts = [sans.variable, mono.variable, 'font-sans'].join(' ')
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html className={`font-sans ${sans.variable} ${mono.variable}`} lang='en' suppressHydrationWarning>
+    <html className={fonts} lang='en' suppressHydrationWarning>
       <body className='flex flex-col min-h-screen'>
         <Providers>{children}</Providers>
       </body>
